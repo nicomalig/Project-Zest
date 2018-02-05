3bf902fc1b140a7fbb3d7ddd5de83a824e9aa414
Nico Malig <nicomalig@gmail.com>
2018-02-04 16:47:54 -0800

    unconnected landing page

--- project-zest/src/App.js
+++ project-zest/src/App.js
@@ -2,19 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
+import Landing from './Landing';
+
+const muiTheme = getMuiTheme({
+   palette: {
+      primary1Color: '#ffaa2d'
+   }
+});
 
 class App extends Component {
-  render() {
-    return (
-      <MuiThemeProvider>
-         <div className="App">
-            <h1>blank space</h1>
-
-         </div>
-      </MuiThemeProvider>
-    );
-  }
+   handleClick = (e) => {
+      e.preventDefault();
+   }
+   
+   render() {
+      return (
+         <MuiThemeProvider muiTheme={muiTheme}>
+            <div className="App flex-container">
+               <div className="flex-row">
+                  <div className="flex-item">
+                     <img src={require('./img/zest.png')} width='118' height='130.25' />
+                  </div>
+                  <Landing handleClick={this.handleClick}/>
+               </div>
+            </div>
+         </MuiThemeProvider>
+      );
+   }
 }
 
 export default App;
---
+++ project-zest/src/Landing.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+
+class Landing extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         email: ''
+      }
+   }
+
+   handleClick = (e) => {
+      e.preventDefault();
+      alert(this.state.email);
+   }
+
+   handleChange = (e, newValue) => {
+      e.preventDefault();
+      this.setState({email: newValue})
+   }
+
+   render() {
+      return (
+         <div className="flex-item">
+            <h2>Don't let cooking be a test, use zest!</h2>
+            <p>We've got cool stuff coming. Stay in the loop!</p>
+            <br />
+            <TextField
+               floatingLabelText="email"
+               value={this.state.value}
+               onChange={this.handleChange}
+            />
+            <br />
+            <RaisedButton label="let's get zesty" onClick={this.handleClick}/>
+
+         </div>
+      );
+   }
+}
+
+export default Landing;
