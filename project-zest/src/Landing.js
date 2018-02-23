9d37dc6b4e85796a8f3587997a36afb13bcaac55
Nico Malig <nicomalig@gmail.com>
2018-02-23 14:47:45 -0800

    Light form validation and error checking

--- project-zest/src/Landing.js
+++ project-zest/src/Landing.js
@@ -16,7 +16,9 @@ class Landing extends Component {
    constructor(props) {
       super(props);
       this.state = {
-         email: ''
+         email: '',
+         errorText: '',
+         displayForm: true
       }
    }
 
@@ -28,12 +30,20 @@ class Landing extends Component {
 
    handleClick = (e) => {
       e.preventDefault();
-      // Verify email address is formatted
 
-      // Store email in firebase database
-      this.writeUserData(this.state.email);
-      this.setState({email: ''});
-      alert(this.state.email + ' saved to Firebase Database');
+      // Verify email address is formatted properly
+      if (this.state.email != "") {
+         // Store email in Firebase database
+         this.writeUserData(this.state.email);
+         this.setState({
+            email: '',
+            errorText: '',
+            displayForm: false
+         });
+         console.log(this.state.email + ' saved!');
+      } else {
+         this.setState({errorText: 'Email cannot be blank'});
+      }
    }
 
    handleChange = (e, newValue) => {
@@ -46,15 +56,26 @@ class Landing extends Component {
          <div className="flex-item">
             <h2>Don't let cooking be a test, use zest!</h2>
             <p>We've got cool stuff coming. Stay in the loop!</p>
-            <br />
-            <TextField
-               floatingLabelText="email"
-               value={this.state.email}
-               onChange={this.handleChange}
-            />
-            <br />
-            <RaisedButton label="let's get zesty" onClick={this.handleClick}/>
+            <p><i> -Team Happy Cappy</i></p>
 
+            {this.state.displayForm &&
+               <div className="form">
+                  <TextField
+                     floatingLabelText="email"
+                     value={this.state.email}
+                     onChange={this.handleChange}
+                     errorText={this.state.errorText}
+                  />
+                  <br />
+                  <RaisedButton label="let's get zesty" onClick={this.handleClick}/>
+                  <br />
+               </div>
+            }
+            {!this.state.displayForm &&
+               <div>
+                  <h4 className="in">You're in! We'll talk to you soon.</h4>
+               </div>
+            }
          </div>
       );
    }
