8fc6ad55909e3b0b33dccb76e0e8634e8863aa1b
Keertana Chandar <kchandar@uw.edu>
2018-02-26 22:55:46 -0800

    facebook login popup shows up

--- project-zest/src/Landing.js
+++ project-zest/src/Landing.js
@@ -9,6 +9,11 @@ var config = {
    databaseURL: "https://zest-de7b0.firebaseio.com",
    storageBucket: "",
 };
+var provider = new firebase.auth.FacebookAuthProvider();
+provider.addScope('email');
+provider.setCustomParameters({
+    'display': 'popup'
+  });
 firebase.initializeApp(config);
 var database = firebase.database();
 
@@ -51,6 +56,26 @@ class Landing extends Component {
       this.setState({email: newValue})
    }
 
+   login() {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });       
+   }
+
+
    render() {
       return (
          <div className="flex-item">
@@ -68,6 +93,9 @@ class Landing extends Component {
                   />
                   <br />
                   <RaisedButton label="let's get zesty" onClick={this.handleClick}/>
+                  <div className="login">
+                    <RaisedButton className="facebook" label="Login with Facebook" onClick={this.login}/>
+                  </div> 
                   <br />
                </div>
             }
