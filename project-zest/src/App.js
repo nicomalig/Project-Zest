99b0a7a0e6ea1c7ec11314c16a0170fcde13d4e1
Nico Malig <nicomalig@gmail.com>
2018-04-05 12:02:32 -0700

    Redux?

--- project-zest/src/App.js
+++ project-zest/src/App.js
@@ -1,35 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Landing from './Landing';
+import * as firebase from "firebase";
+
+var config = {
+    apiKey: "AIzaSyABka5H54O_iaPXXm16sW2b_7PmkybOfP8",
+    authDomain: "zest-de7b0.firebaseapp.com",
+    databaseURL: "https://zest-de7b0.firebaseio.com",
+    projectId: "zest-de7b0",
+    storageBucket: "zest-de7b0.appspot.com",
+    messagingSenderId: "173225663428"
+  };
+  
+ firebase.initializeApp(config);
+ var database = firebase.database();
 
 const muiTheme = getMuiTheme({
-   palette: {
-      primary1Color: '#ffaa2d'
-   }
+	palette: {
+		primary1Color: '#ffaa2d'
+	}
 });
 
-class App extends Component {
-   handleClick = (e) => {
-      e.preventDefault();
-   }
 
-   render() {
-      return (
-         <MuiThemeProvider muiTheme={muiTheme}>
-            <div className="App flex-container">
-               <div className="flex-row">
-                  <div className="flex-item">
-                     <img src={require('./img/zest.png')} width='118' height='130.25' />
-                  </div>
-                  <Landing handleClick={this.handleClick}/>
-               </div>
-            </div>
-         </MuiThemeProvider>
-      );
-   }
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+	handleClick = (e) => {
+		e.preventDefault();
+	}
+	
+	render() {
+		return (
+			<MuiThemeProvider muiTheme={muiTheme}>
+				<div className="App flex-container">
+					<div className="flex-row">
+						<div className="flex-item">
+                            <img src={require('./img/zest.png')} 
+                                width='118' height='130.25' alt="zest logo" />
+						</div>
+						<Landing handleClick={this.handleClick} db={database} />
+					</div>
+				</div>
+			</MuiThemeProvider>
+		);
+	}
 }
 
 export default App;
