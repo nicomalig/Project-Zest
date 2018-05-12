23675d0aff0482bb6f12f52c4e83993cacf52175
Nico Malig <nicomalig@gmail.com>
2018-05-12 16:17:14 -0700

    home screen
    NOT responsive (yet!)

--- project-zest/src/HomePage.js
+++ project-zest/src/HomePage.js
@@ -4,62 +4,71 @@ import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 import LogInWithFacebookButton from "./LogInWithFacebookButton";
 import HomeScreenSearchBar from "./HomeScreenSearchBar";
+import logo from "./img/zest.png";
+
 
 const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: "#ffaa2d"
-  }
+   palette: {
+      primary1Color: "#ffaa2d"
+   }
 });
 
 class HomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null
-    };
-  }
+   constructor(props) {
+      super(props);
+      this.state = {
+         user: null
+      };
+   }
 
-  handleClick = e => {
-    e.preventDefault();
-  };
+   handleClick = e => {
+      e.preventDefault();
+   };
 
-  render() {
-    return (
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <div className="flex-container" id="main-screen">
-          <div className="flex-row">
-            <div className="flex-item">
-              <HomeScreenSearchBar handler={this.props.handler} />
+   render() {
+      return (
+         <MuiThemeProvider muiTheme={muiTheme}>
+            <div className="flex-container" id="main-screen">
+               <div className="flex-container-left">
+
+                  <div className="hs-logo-container flex-item">
+                     <img src={logo} alt="logo"/>
+                  </div>
+
+                  <div className="flex-item">
+                     <div>
+                        <h2>Cook with Zest</h2>
+                        <p>
+                           We make it easy for you to make conversions, cook for any
+                           number of people, and adjust any recipe to your needs!
+                        </p>
+                        <p>
+                           <i>Don't let cooking be a test, use Zest!</i>
+                        </p>
+                     </div>
+                  </div>
+
+                  <div className="flex-item">
+                     <HomeScreenSearchBar handler={this.props.handler} />
+                  </div>
+
+               </div>
+
+               <div id="vertical-line" />
+
+               <div className="flex-container-right">
+                  <div id="login-div">
+                     <h2>LOGIN</h2>
+                     <h4>Want to <i>save</i> your recipes?</h4>
+                     <div className="flex-item">
+                        <LogInWithFacebookButton handler={this.handler} />
+                     </div>
+                  </div>
+               </div>
             </div>
-          </div>
-          <div className="flex-row">
-            <div className="flex-item">
-              <div>
-                <h2>Cook with Zest</h2>
-                <p>
-                  We make it easy for you to make conversions, cook for any
-                  number of people, and adjust any recipe to your needs!
-                </p>
-                <p>
-                  <i>Don't let cooking be a test, use Zest!</i>
-                </p>
-              </div>
-            </div>
-            <div id="vertical-line" />
-            <div className="flex-item">
-              <div id="login-div" className="flex-item">
-                <h2>LOGIN</h2>
-                <h4>
-                  Want to <i id="login-save">save</i> your recipes?
-                </h4>
-                <LogInWithFacebookButton handler={this.handler} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </MuiThemeProvider>
-    );
-  }
+         </MuiThemeProvider>
+      );
+   }
 }
 
 export default HomePage;
--- project-zest/src/HomeScreenSearchBar.js
+++ project-zest/src/HomeScreenSearchBar.js
@@ -2,48 +2,54 @@ import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import RaisedButton from "material-ui/RaisedButton";
 import IsUrl from "is-url";
+import './App.css';
 
 class HomeScreenSearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      url: "",
-      errorText: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+   constructor(props) {
+      super(props);
+      this.state = {
+         url: "",
+         errorText: ""
+      };
+      this.handleChange = this.handleChange.bind(this);
+   }
 
-  handleChange(e, newValue) {
-    e.preventDefault();
-    console.log(newValue);
-    this.setState({ url: newValue });
-  }
+   handleChange(e, newValue) {
+      e.preventDefault();
+      console.log(newValue);
+      this.setState({ url: newValue });
+   }
 
-  onSearchClick = e => {
-    e.preventDefault();
-    // Verify email address is formatted properly
-    if (IsUrl(this.state.url)) {
-      this.setState({ errorText: "" });
-      this.props.handler(e, { url: this.state.url, goToRecipePage: true });
-    } else {
-      this.setState({ errorText: "Enter a valid URL" });
-    }
-  };
+   onSearchClick = e => {
+      e.preventDefault();
+      // Verify email address is formatted properly
+      if (IsUrl(this.state.url)) {
+         this.setState({ errorText: "" });
+         this.props.handler(e, { url: this.state.url, goToRecipePage: true });
+      } else {
+         this.setState({ errorText: "Enter a valid URL" });
+      }
+   };
 
-  render() {
-    return (
-      <div className="form flex-item">
-        <TextField
-          floatingLabelText="Paste recipe URL here"
-          value={this.state.url}
-          onChange={this.handleChange}
-          errorText={this.state.errorText}
-        />
-        <br />
-        <RaisedButton label="Start Cooking!" onClick={this.onSearchClick} />
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div className="form flex-item">
+            <TextField
+               className="hs-search"
+               floatingLabelText="Paste Food Network URL here"
+               value={this.state.url}
+               onChange={this.handleChange}
+               errorText={this.state.errorText}
+               fullWidth={true}
+               underlineStyle={{
+                  borderColor: "#B06800", /* a bit darker */
+               }}
+            />
+            <br />
+            <RaisedButton label="Start Cooking!" onClick={this.onSearchClick} />
+         </div>
+      );
+   }
 }
 
 export default HomeScreenSearchBar;
