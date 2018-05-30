4031316423065446557d5893922b70957ca94042
Keertana Chandar <kchandar@uw.edu>
2018-05-29 19:10:52 -0700

    show saved recipes on one click

--- project-zest/src/InvalidURLRecipe.js
+++ project-zest/src/InvalidURLRecipe.js
@@ -11,88 +11,65 @@ import MainScreenSearchBar from "./MainScreenSearchBar";
 import FlatButton from "material-ui/FlatButton/FlatButton";
 
 const muiTheme = getMuiTheme({
-   palette: {
-      primary1Color: "#ffaa2d"
-   }
+  palette: {
+    primary1Color: "#ffaa2d"
+  }
 });
 
 class InvalidURLRecipe extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         url: "",
-         renderSaved: false,
-         actuallyRender: false,
-         fluff: false,
-      };
-      this.recipeHandler = this.recipeHandler.bind(this)
-      this.handleSavedClick = this.handleSavedClick.bind(this)
-      this.actually = this.actually.bind(this)
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: "",
+      renderSaved: false
+    };
+    this.recipeHandler = this.recipeHandler.bind(this);
+    this.handleSavedClick = this.handleSavedClick.bind(this);
+    this.actually = this.actually.bind(this);
+  }
 
-   handleClick = e => {
-      e.preventDefault();
-   };
+  handleClick = e => {
+    e.preventDefault();
+  };
 
-   handleSavedClick = e => {
-      e.preventDefault()
-      this.setState({ renderSaved: !this.state.renderSaved })
-   }
+  handleSavedClick = e => {
+    e.preventDefault();
+    this.setState({ renderSaved: !this.state.renderSaved });
+  };
 
-   recipeHandler(e, newState) {
-      e.preventDefault();
-      this.setState(newState);
-   }
+  recipeHandler(e, newState) {
+    e.preventDefault();
+    this.setState(newState);
+  }
 
-   actually() {
-      this.setState({
-         renderSaved: true,
-         actuallyRender: false,
-      })
-      setTimeout(() => {
-         this.setState({
-            actuallyRender: true,
-         })
-      }, 500);
-   }
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div className="flex-container-m">
+          <div className="mssb">
+            <MainScreenSearchBar
+              handler={this.props.handler}
+              url={this.props.url}
+              user={this.props.user}
+              renderSaved={this.state.renderSaved}
+              handleSavedClick={this.handleSavedClick}
+              recipeHandler={this.recipeHandler}
+            />
+          </div>
 
-   render() {
-      return (
-         <MuiThemeProvider muiTheme={muiTheme}>
-            <div className="flex-container-m">
-               <div className="mssb">
-                  <MainScreenSearchBar
-                     handler={this.props.handler}
-                     url={this.props.url}
-                     user={this.props.user}
-                     renderSaved={this.state.renderSaved}
-                     handleSavedClick={this.handleSavedClick}
-                     recipeHandler={this.recipeHandler}
-
-                  />
-               </div>
-
-               {/* DISPLAY SAVED RECIPES */}
-               {this.state.renderSaved && this.state.actuallyRender &&
-                  <div className="flex-container-content">
-                     <SavedRecipes
-                        user={this.props.user}
-                        handler={this.props.handler}
-                     />
-                  </div>
-               }
-               {this.state.renderSaved && !this.state.actuallyRender &&
-                  <div className="flex-container-content">
-                     <RaisedButton
-                        label="Click here to display"
-                        onClick={this.actually}
-                     />
-                  </div>
-               }
+          {/* DISPLAY SAVED RECIPES */}
+          {this.state.renderSaved && (
+            <div className="flex-container-content">
+              <SavedRecipes
+                user={this.props.user}
+                handler={this.props.handler}
+              />
             </div>
-         </MuiThemeProvider>
-      );
-   }
+          )}
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default InvalidURLRecipe;
--- project-zest/src/SavedRecipes.js
+++ project-zest/src/SavedRecipes.js
@@ -6,84 +6,85 @@ import SignOutButton from "./SignOutButton";
 import RaisedButton from "material-ui/RaisedButton";
 import SavedRecipeCard from "./SavedRecipeCard";
 import { firebase } from "./FirebaseConfig";
-import './SavedRecipeCard.css'
+import "./SavedRecipeCard.css";
 import { log } from "util";
 
 const muiTheme = getMuiTheme({
-   palette: {
-      primary1Color: "#ffaa2d"
-   }
+  palette: {
+    primary1Color: "#ffaa2d"
+  }
 });
 
 class SavedRecipes extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         savedRecipes: []
-      };
-      this.getSavedRecipes = this.getSavedRecipes.bind(this);
-      this.updatePage = this.updatePage.bind(this);
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      savedRecipes: []
+    };
+    this.getSavedRecipes = this.getSavedRecipes.bind(this);
+    this.updatePage = this.updatePage.bind(this);
+  }
 
-   handleClick = e => {
-      e.preventDefault();
-   };
+  handleClick = e => {
+    e.preventDefault();
+  };
 
-   componentDidMount() {
-      this.getSavedRecipes()
-   }
+  componentDidMount() {
+    this.getSavedRecipes();
+  }
 
-   getSavedRecipes() {
-      let currentUser = firebase.auth().currentUser;
-      if (currentUser == null) {
-         return false;
-      }
-      var ref = firebase.database().ref("users/" + currentUser.uid);
-      var saved = [];
-      ref.on("value", function (snapshot) {
-         snapshot.forEach(function (linkSnapshot) {
-            var data = linkSnapshot.val();
-            saved.push(
-               <SavedRecipeCard
-                  link={data.link}
-                  name={data.data.name}
-                  image={data.data.img}
-                  key={linkSnapshot.key}
-               />
-            );
-            return;
-         });
-         // updatePage(saved);
+  getSavedRecipes() {
+    let currentUser = firebase.auth().currentUser;
+    if (currentUser == null) {
+      return false;
+    }
+    var ref = firebase.database().ref("users/" + currentUser.uid);
+    var saved = [];
+    ref.on("value", function(snapshot) {
+      snapshot.forEach(function(linkSnapshot) {
+        var data = linkSnapshot.val();
+        saved.push(
+          <SavedRecipeCard
+            link={data.link}
+            name={data.data.name}
+            image={data.data.img}
+            key={linkSnapshot.key}
+          />
+        );
+        return;
       });
+      // updatePage(saved);
+    });
+    setTimeout(() => {
       this.updatePage(saved);
+    }, 100);
+    return saved;
+  }
 
-      return saved;
-   }
+  updatePage(saved) {
+    this.setState({
+      savedRecipes: saved
+    });
+  }
 
-   updatePage(saved) {
-      this.setState({
-         savedRecipes: saved
-      });
-   }
-
-   render() {
-      return (
-         <div>
-            {this.props.user && (
-               <MuiThemeProvider muiTheme={muiTheme}>
-                  <div>
-                     <h2 className="sc-header">
-                        Saved Recipes for {this.props.user.displayName}
-                     </h2>
-                     <div className="saved-card-container">
-                        {this.state.savedRecipes}
-                     </div>
-                  </div>
-               </MuiThemeProvider>
-            )}
-         </div>
-      );
-   }
+  render() {
+    return (
+      <div>
+        {this.props.user && (
+          <MuiThemeProvider muiTheme={muiTheme}>
+            <div>
+              <h2 className="sc-header">
+                Saved Recipes for {this.props.user.displayName}
+              </h2>
+              <div className="saved-card-container">
+                {this.state.savedRecipes}
+              </div>
+            </div>
+          </MuiThemeProvider>
+        )}
+      </div>
+    );
+  }
 }
 
 export default SavedRecipes;
